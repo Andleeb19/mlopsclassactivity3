84606ce876a22abef96703af76630bd124553f5b
Andleeb19 <andleebzahra191@gmail.com>
2025-11-12 20:28:59 +0500

    Update preprocess.py
    Create data directory in preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,11 @@
+import os
 import pandas as pd
 from sklearn.datasets import load_iris
 
 iris = load_iris()
 data = pd.DataFrame(iris.data, columns=iris.feature_names)
 data['target'] = iris.target
-
+# Create data directory if it doesn't exist
+os.makedirs("data", exist_ok=True)
 data.to_csv("data/preprocessed.csv", index=False)
 print("✅ Data preprocessed and saved to data/preprocessed.csv")
